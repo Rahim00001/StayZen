07002f35d3a7cb74f762e9484789f32dc6d5eaaf
Rahim00001 <rahimlpt2022@gmail.com>
2023-11-07 07:37:00 +0600

    validation added on registation form

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import registerImg from "../../../public/register.jpg"
 import { AuthContext } from "../../providers/AuthProviders";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 const Register = () => {
 
     const { createUser } = useContext(AuthContext);
+    const [registerError, setRegisterError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const handleRegister = e => {
         e.preventDefault();
@@ -15,13 +17,31 @@ const Register = () => {
         const password = form.password.value;
         console.log(name, email, photo, password);
 
+        setRegisterError('');
+        setSuccess('');
+
+        if (password.length < 6) {
+            setRegisterError('Password should be at least 6 characters or longer.');
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setRegisterError('Password Should have at least one uppercase characters');
+            return
+        }
+        else if (!/[!@#$%^&*]/.test(password)) {
+            setRegisterError('Password Should have at least one special characters');
+            return
+        }
+
         // create User
         createUser(email, password)
             .then(result => {
                 console.log(result.user);
+                setSuccess('Successfully Registerd')
             })
             .catch(error => {
                 console.error(error);
+                setRegisterError(error.message);
             })
     }
     return (
@@ -56,14 +76,17 @@ const Register = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                            {/* <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                    </label> */}
                         </div>
                         <div className="form-control mt-6">
-                            <input type="submit" value="REgister" className="btn bg-red-500 text-white" />
+                            <input type="submit" value="register" className="btn bg-red-500 text-white" />
                         </div>
                         <p className="text-center my-4">Already Have An Account? <Link to='/login' className="text-red-600 font-bold">Login</Link></p>
+                        {
+                            registerError && <p className="text-red-600">{registerError}</p>
+                        }
+                        {
+                            success && <p className="text-green-600">{success}</p>
+                        }
                     </form>
                 </div>
             </div>
