8045019b3e8b5cb82ff36f3374169ac727f99cd5
Rahim00001 <rahimlpt2022@gmail.com>
2023-11-09 14:19:01 +0600

    axiox pakage installed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -15,7 +15,6 @@ const Login = () => {
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
         signInUser(email, password)
             .then(result => {
                 console.log(result.user);
